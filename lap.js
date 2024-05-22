a72893f0d53dbcf75ae75aec8a981580a025a8e8
sult <168359467+Salbdr@users.noreply.github.com>
2024-05-22 20:30:04 +0300

    new map

--- lap.js
+++ lap.js
@@ -1,64 +1,97 @@
-function sum( num1 , num2){
+// function sum( num1 , num2){
 
 
-return num1+num2;
+// return num1+num2;
 
 
+// }
+
+
+
+
+// console.log(sum(2,2));
+
+
+
+// function minc( num1 , num2){
+
+
+//     return num1-num2;
+    
+    
+//     }
+    
+    
+    
+    
+//     console.log(minc(2,2));
+
+
+
+
+// function aaa(num1 , num2){
+
+// return num1*num2;
+
+
+// }
+
+
+
+
+// console.log(aaa(2,2));
+
+
+
+
+
+
+
+// function bbb(num1,num2){
+
+//     return num1 /num2
+// }
+
+
+
+// console.log(bbb(2,2));
+    
+    
+
+
+
+
+
+
+
+
+// var classrome=["sul" , "ahmad" , "ali", 55];
+// console.log(classrome);
+
+// classrome[2]="saad";        //تعديل بداخل المصفوفة
+// console.log(classrome);
+
+
+
+// var classrome=["sul" , "ahmad" , "ali"];
+// console.log(classrome);
+// classrome.splice(1,2): // لحذف احمد وعلي
+// classrome.splice(1,0, "sul , 11") //لاضافة عناصر بعد احمد سيتم اضافة سلط و11
+
+// console.log(classrome.sort()); // لترتيب العناصر داخل المصفوفه مثل الرموز اولا بعدين الرقام  وبعدين الحروف وكذا
+
+
+// console.log([0 ,1]);//لايجاد عنصر داخل المصفوفة
+
+var num1=[1,2,3,4,5,6,7,8,9];
+var num2=[]
+console.log(num1);
+
+for (var i=0 ; i<num1.length; i++){
+    num2.push(num1)
 }
 
-
-
-
-console.log(sum(2,2));
-
-
-
-function minc( num1 , num2){
-
-
-    return num1-num2;
-    
-    
-    }
-    
-    
-    
-    
-    console.log(minc(2,2));
-
-
-
-
-function aaa(num1 , num2){
-
-return num1*num2;
-
-
-}
-
-
-
-
-console.log(aaa(2,2));
-
-
-
-
-
-
-
-function bbb(num1,num2){
-
-    return num1 /num2
-}
-
-
-
-console.log(bbb(2,2));
-    
-    
-
-
+console.log(num2);
 
 
 
