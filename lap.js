2cdd4fb0bb2194e874841794235f53a5bd17ae6c
sult <168359467+Salbdr@users.noreply.github.com>
2024-05-22 18:38:39 +0300

    function

---
+++ lap.js
@@ -0,0 +1,66 @@
+function sum( num1 , num2){
+
+
+return num1+num2;
+
+
+}
+
+
+
+
+console.log(sum(2,2));
+
+
+
+function minc( num1 , num2){
+
+
+    return num1-num2;
+    
+    
+    }
+    
+    
+    
+    
+    console.log(minc(2,2));
+
+
+
+
+function aaa(num1 , num2){
+
+return num1*num2;
+
+
+}
+
+
+
+
+console.log(aaa(2,2));
+
+
+
+
+
+
+
+function bbb(num1,num2){
+
+    return num1 /num2
+}
+
+
+
+console.log(bbb(2,2));
+    
+    
+
+
+
+
+
+
+
